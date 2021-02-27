889f0cfe321b8f959652601b2d511e44ea4f073d
nsecuritysg <40561053+nsecuritysg@users.noreply.github.com>
2021-02-27 16:52:57 +0300

    add Step2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Step1 } from './Step1';
+import { Step2 } from './Step2';
 
-const Step2 = () => <>Step 2</>;
 const Step3 = () => <>Step 3</>;
 const Result = () => <>Result</>;
 
---
+++ src/Step2.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { MainContainer } from './components/MainContainer';
+import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router-dom';
+import { Form } from './components/Form';
+import { Input } from './components/Input';
+import { useForm } from 'react-hook-form';
+import { PrimaryButton } from './components/PrimaryButton';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import parsePhoneNumberFromString from 'libphonenumber-js';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Email should have correct format')
+    .required('Email is a required field'),
+});
+
+const normalizePhoneNumber = (value) => {
+  const phoneNumber = parsePhoneNumberFromString(value);
+  if (!phoneNumber) {
+    return value;
+  }
+  return phoneNumber.formatInternational();
+};
+
+export const Step2 = () => {
+  const history = useHistory();
+  const { register, handleSubmit, errors, watch } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  });
+
+  const hasPhone = watch('hasPhone');
+
+  const onSubmit = (data) => {
+    history.push('./step3');
+  };
+
+  return (
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        ✈️ Step 2
+      </Typography>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          ref={register}
+          id="email"
+          type="email"
+          label="Email"
+          name="email"
+          error={!!errors.email}
+          helperText={errors?.email?.message}
+          required
+        />
+        <FormControlLabel
+          control={
+            <Checkbox name="hasPhone" inputRef={register} color="primary" />
+          }
+          label="Do you have a phone?"
+        />
+        {hasPhone && (
+          <Input
+            ref={register}
+            id="phoneNumber"
+            type="tel"
+            label="Phone Number"
+            name="phoneNumber"
+            onChange={(event) => {
+              event.target.value = normalizePhoneNumber(event.target.value);
+            }}
+          />
+        )}
+        <PrimaryButton>Next</PrimaryButton>
+      </Form>
+    </MainContainer>
+  );
+};
