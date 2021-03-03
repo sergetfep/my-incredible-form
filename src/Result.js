6cffe2b82905f5ce002e38ffecfff298d4d449e6
nsecuritysg <40561053+nsecuritysg@users.noreply.github.com>
2021-03-03 14:33:10 +0300

    add Result component, server, other libraries
    add Result component, server, libraries (cors, express, express-fileupload, morgan)

--- src/Result.js
+++ src/Result.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainContainer } from './components/MainContainer';
 import Typography from '@material-ui/core/Typography';
 import { useData } from './DataContext';
@@ -18,6 +18,9 @@ import {
 import { Link } from 'react-router-dom';
 import { InsertDriveFile } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import { PrimaryButton } from './components/PrimaryButton';
+import Swal from 'sweetalert2';
+import Confetti from 'react-confetti';
 
 const useStyles = makeStyles({
   root: {
@@ -29,10 +32,38 @@ const useStyles = makeStyles({
 });
 
 export const Result = () => {
+  const [success, setSuccess] = useState(false);
   const styles = useStyles();
   const { data } = useData();
   const entries = Object.entries(data).filter((entry) => entry[0] !== 'files');
   const { files } = data;
+
+  const onSubmit = async () => {
+    const formData = new FormData();
+
+    if (data.files) {
+      data.files.forEach((file) => {
+        formData.append('files', file, file.name);
+      });
+    }
+    entries.forEach((entry) => {
+      formData.append(entry[0], entry[1]);
+    });
+
+    const res = await fetch('http://localhost:4000', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (res.status === 200) {
+      Swal.fire('Great job!', "You're the best!", 'success');
+      setSuccess(true);
+    }
+  };
+
+  if (success) {
+    return <Confetti />;
+  }
 
   return (
     <MainContainer>
@@ -60,7 +91,7 @@ export const Result = () => {
       {files && (
         <>
           <Typography component="h2" variant="h5">
-            👉👌 Files
+            👌 Files
           </Typography>
           <List>
             {files.map((f, index) => (
@@ -74,6 +105,7 @@ export const Result = () => {
           </List>
         </>
       )}
+      <PrimaryButton onClick={onSubmit}>Submit</PrimaryButton>
       <Link to="/">Start over</Link>
     </MainContainer>
   );
